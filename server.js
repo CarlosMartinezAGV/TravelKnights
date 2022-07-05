776e71e1d1ed0f20c7dfd1299faa456590726cdb
Carlos Martinez <cmartinez98@knights.ucf.edu>
2022-07-05 16:50:31 -0400

    edited whitespace

--- server.js
+++ server.js
@@ -2,17 +2,17 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-const path = require('path');
+const path = require("path");
 const PORT = process.env.PORT || 5000;
-
 const app = express();
 
-app.set('port', (process.env.PORT || 5000));
+app.set("port", process.env.PORT || 5000);
 
 app.use(cors());
 app.use(bodyParser.json());
-const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb+srv://Carlos:solraC@cluster0.ukckn.mongodb.net/?retryWrites=true&w=majority';
+const MongoClient = require("mongodb").MongoClient;
+const url =
+  "mongodb+srv://Carlos:solraC@cluster0.ukckn.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(url);
 client.connect();
 app.use((req, res, next) => {
@@ -28,17 +28,14 @@ app.use((req, res, next) => {
   next();
 });
 
-if (process.env.NODE_ENV === 'production') 
-{
+if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static('frontend/build'));
-  app.get('*', (req, res) => 
- {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  app.use(express.static("frontend/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
 }
 
-app.listen(PORT, () => 
-{
-  console.log('Server listening on port ' + PORT);
+app.listen(PORT, () => {
+  console.log("Server listening on port " + PORT);
 });
