e2d24034e7237769bdad88102571436e7162c9e9
BRYAN CABRERA <BRYANMCABRERA@GMAIL.COM>
2022-07-05 16:37:10 -0400

    Added required heroku information

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
 const app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
 app.use(cors());
 app.use(bodyParser.json());
 const MongoClient = require('mongodb').MongoClient;
@@ -20,4 +27,18 @@ app.use((req, res, next) => {
   );
   next();
 });
-app.listen(5000);
+
+if (process.env.NODE_ENV === 'production') 
+{
+  // Set static folder
+  app.use(express.static('frontend/build'));
+  app.get('*', (req, res) => 
+ {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
+
+app.listen(PORT, () => 
+{
+  console.log('Server listening on port ' + PORT);
+});
