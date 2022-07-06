27c1805891a619594a551c716414508dd8c56dde
Carlos Martinez <cmartinez98@knights.ucf.edu>
2022-07-05 21:41:40 -0400

    trying to connect heroku and changing ports

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const path = require("path");
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.set("port", process.env.PORT || 5000);
+app.set("port", process.env.PORT || 3000);
 
 app.use(cors());
 app.use(bodyParser.json());
